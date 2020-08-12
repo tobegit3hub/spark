2e8d97397f66328b19f2c7a38c2bfabdf16c90be
tobe <tobeg3oogle@gmail.com>
2020-08-12 16:19:04 +0800

    Change spark version from 3.0.1-snapshot to 3.0.0

--- upload_spark_distribution.sh
+++ upload_spark_distribution.sh
@@ -4,9 +4,9 @@ set -x -e
 
 URL="http://pkg.4paradigm.com:81/fedb/spark_dailybuild/"
 VERSION=`date +"%Y-%m-%d"`-`git rev-parse --short HEAD`
-FILE=fespark-3.0.1-bin-$VERSION.tgz
+FILE=fespark-3.0.0-bin-$VERSION.tgz
 
-mv ./spark-3.0.1-SNAPSHOT-bin-llvm-spark.tgz $FILE
+mv ./spark-3.0.0-bin-llvm-spark.tgz $FILE
 echo "Uploading $FILE to $URL"
 
 CSRF=$(curl $URL 2>/dev/null| grep '"token"' | sed 's:.*"token">\(.*\)</span>:\1:')
